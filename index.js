eb2a293cdaca253a1fd92bf4d05f0417610422e0
Megan Horsfield <meganthadev@gmail.com>
2020-08-07 00:20:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function Scooter(year, color, model){
-  return {
-    year, 
-    color,
-    model,
-  }
+    this.year = year;
+    this.color = color;
+    this.model = model;
 }
 
 function Driver (name, age, experience ){
